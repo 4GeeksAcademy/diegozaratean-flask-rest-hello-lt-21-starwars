6aa1fe373847afc4268c9c92892badb46a4d63eb
diego <diegozaratean@gmail.com>
2023-10-17 01:50:44 +0000

    finish auth class

--- src/app.py
+++ src/app.py
@@ -11,6 +11,11 @@ from admin import setup_admin
 from models import db, User, Estudio
 #from models import Person
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
@@ -25,6 +30,10 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "cualquier ocsa que se te ocurra"  # Change this! 1234abc.   ksdjfklasjfkasjfljaskfjasdkljflafdsfds.  123abd. jhaskdfhkajdshfkjashfkjashfkjashdk
+jwt = JWTManager(app)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -83,6 +92,49 @@ def crear_estudio():
     }
 
     return jsonify(response_body), 200
+##.  rafael@gmial,. 123. asdkfjkalksfjlkasdjflkasdj === asdkfjkalksfjlkasdjflkasdj
+###  david#gmail. 456.    iewuruqwruqwuruqweruqweu
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    # busque el usuario con email email 
+    user = User.query.filter_by(email=email).first()
+    
+    if user is None:
+        return jsonify({"msg": "The user isn't in the system"}), 401
+
+    print(user)
+    print(user.serialize())
+    print(user.email)
+    print(user.password)
+    
+    if password != user.password :
+        return jsonify({"msg": "Bad password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@app.route("/profile", methods=["GET"])
+@jwt_required()
+def profile():
+    # Access the identity of the current user with get_jwt_identity
+    user_email = get_jwt_identity()
+    # return jsonify(logged_in_as=user_email), 200
+    user = User.query.filter_by(email=user_email).first()
+    print(user)
+    response_body = {
+        "msg": "Usuario encontrado",
+        "user": user.serialize()
+    }
+
+    return jsonify(response_body), 200
+
 
 # FINAL DE CODIGO
 
